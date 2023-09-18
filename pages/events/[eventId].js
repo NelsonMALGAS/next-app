6b437b41f5d0e45c0ecb73a7d18fb85f37c06cc0
NelsonMALGAS <zmalgas69@gmail.com>
2023-09-18 09:07:04 +0200

    Adds getStaticProps & static paths fns

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,35 +1,16 @@
-import { useRouter } from "next/router";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment } from "react";
 import ErrorAlert from "@/Components/UI/error-alert/error-alert";
 import Button from "@/Components/UI/button";
 import Comments from "@/Components/input/comments";
-import { getEventById } from "@/Dummy";
+import { getEventById, getAllEventIds } from "@/Dummy";
 import EventSummary from "@/Components/event-detail/event-summary";
 import EventLogistics from "@/Components/event-detail/event-logistics";
 import EventContent from "@/Components/event-detail/event-content";
 
-export default function EventDetailPage() {
-	const router = useRouter();
-	const id = router.query.eventId;
-	const [event, setEvent] = useState(null);
+export default function EventDetailPage(props) {
+	const { event, error } = props;
 
-	useEffect(() => {
-		const fetchEvent = async () => {
-			try {
-				const eventData = await getEventById(id);
-				setEvent(eventData);
-			} catch (error) {
-				console.error("Error fetching event:", error);
-				setEvent(null);
-			}
-		};
-
-		if (id) {
-			fetchEvent();
-		}
-	}, [id]);
-
-	if (!event) {
+	if (error) {
 		return (
 			<Fragment>
 				<ErrorAlert>
@@ -57,4 +38,42 @@ export default function EventDetailPage() {
 			<Comments eventId={event.id} />
 		</Fragment>
 	);
+}
+
+// Fetch data at build time
+export async function getStaticProps(context) {
+	const eventId = context.params.eventId;
+	try {
+		const eventData = await getEventById(eventId);
+		if (!eventData) {
+			return {
+				props: {
+					error: true,
+				},
+			};
+		}
+		return {
+			props: {
+				event: eventData,
+				error: false,
+			},
+		};
+	} catch (error) {
+		return {
+			props: {
+				error: true,
+			},
+		};
+	}
+}
+
+export async function getStaticPaths() {
+	const allEventIds = await getAllEventIds(); 
+	const paths = allEventIds.map((eventId) => ({
+		params: { eventId },
+	}));
+	return {
+		paths,
+		fallback: false, 
+	};
 }
